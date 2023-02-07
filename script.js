efa08996c689adc9c14dfe690f27e6f2b4fb1ae1
yang <joey674901205@gmail.com>
2023-02-07 16:48:48 -0500

    create script.js

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-if (window.screen.availWidth === 375) {
-  const banner = document.querySelector(".banner");
-  const menu = document.querySelector(".menu");
-  const nav = document.querySelector(".nav");
-  const menuBtn = document.createElement("div");
-  const triangle = document.createElement("div");
+const banner = document.querySelector(".banner");
+const menu = document.querySelector(".menu");
+const nav = document.querySelector(".nav");
+const menuBtn = document.createElement("div");
+const triangle = document.createElement("div");
 
-  menu.classList.add("hidden");
-  menuBtn.classList.add("btn-menu");
-  triangle.classList.add("triangle");
+function resize() {
+  if (
+    // window.resizeTo(window.screen.availWidth <= 375, window.screen.availHeight)
+    // ||
+    window.screen.availWidth <= 375
+  ) {
+    menu.classList.add("hidden");
+    menuBtn.classList.add("btn-menu");
+    triangle.classList.add("triangle");
 
-  banner.appendChild(menuBtn);
-  nav.appendChild(triangle);
+    banner.appendChild(menuBtn);
 
-  menuBtn.addEventListener("click", function clickBtn() {
-    menu.classList.toggle("hidden");
-  });
+    menuBtn.addEventListener("click", function clickBtn() {
+      menu.classList.toggle("hidden");
+      nav.appendChild(triangle);
+    });
+  } else {
+    banner.removeChild(banner.lastChild);
+    nav.removeChild(nav.lastChild);
+  }
 }
+
+resize();
